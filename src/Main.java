f32333b9fec443b6699c3e1a9ddca2478b411b3c
OwgerD <113381416+OwgerD@users.noreply.github.com>
2022-10-19 14:39:52 -0400

    Update Main.java
    Changes

--- src/Main.java
+++ src/Main.java
@@ -27,50 +27,77 @@ public class Main {
 		int hund =(int) change/100;
 		System.out.println("$100s:"+hund);
 		change=change-hund*100;
-	}if(change / 50>=1) {
+		change=Math.round(change*100)/100.0;
+	}
+	if(change / 50>=1) {
 		int fifty =(int) change/50;
 		System.out.println("$50s:"+fifty);
 		change=change-fifty*50;
-	}if(change / 20>=1) {
+		change=Math.round(change*100)/100.0;
+	}
+	if(change / 20>=1) {
 		int twen =(int) change/20;
 		System.out.println("$20s:"+twen);
 		change=change-twen*20;
-	}if(change / 10>=1) {
+		change=Math.round(change*100)/100.0;
+	}
+	if(change / 10>=1) {
 		int ten =(int) change/10;
 		System.out.println("$10s:"+ten);
 		change=change-ten*10;
-	}if(change / 5>=1) {
+		change=Math.round(change*100)/100.0;
+	}
+	if(change / 5>=1) {
 		int five =(int) change/5;
 		System.out.println("$5s:"+five);
 		change=change-five*5;
-	}if(change / 2>=1) {
+		change=Math.round(change*100)/100.0;
+	}
+	if(change / 2>=1) {
 		int two =(int) change/2;
 		System.out.println("$2s:"+ two);
 		change=change-two*2;
-	}if(change / 1>=1) {
+		change=Math.round(change*100)/100.0;
+	}
+	if(change / 1>=1) {
 		int one =(int) change/1;
 		System.out.println("$1s:"+ one);
 		change=change-one*1;
-	}if(change / 0.25>=1) {
+		change=Math.round(change*100)/100.0;
+	}
+	if(change / 0.25>=1) {
 		int quart = (int) (change/0.25);
 		System.out.println("$0.25s:"+ quart);
 		change=change-quart*0.25;
-	}if(change / 0.10>=1) {
-		int dime = (int)(change/0.10);
-		System.out.println("$0.10s:"+ (int)dime);
+		change=Math.round(change*100)/100.0;
+	}	
+	
+	if(change / 0.10>=1) {
+		int dime = (int) (change/0.10);
+		System.out.println("$0.10s:"+ dime);
 		change=change-dime*0.10;
-	}
-	
-	if(change / 0.05<=1) {
-		int Nickels = (int) (change/0.05);
-		
-		System.out.println("$0.05s:"+ Nickels);
-		change=change-Nickels*0.05;
+		change=Math.round(change*100)/100.0;
+	}		
+	if(change / 0.05>=1) {
+		int Nick = (int) (change/0.05);
+		System.out.println("$0.05s:"+ Nick);
+		change=change-Nick*0.05;
+	}	
 
+	int leftover = (int)((change*100)%10);
+	if (leftover==1 || leftover ==2) {
+		change=change-(leftover/100.0);
+	}else if (leftover==3 || leftover==4) {
+		change=change + ((5-leftover)/100.0);
 	}
+	
+
+
+	
 		//As a bonus, make your project round to the nearest $0.05 like actual stores.
-	
+
 		in.close();
-	}
 	
+	
+}
 }
